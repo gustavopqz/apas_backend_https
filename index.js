b3f0a2ba5f227328b074308bcbea764379011a64
Gustavo Pasqua <dev.gustavo@outlook.com>
2024-06-11 18:28:02 -0300

    fix: Teste

--- index.js
+++ index.js
@@ -8,28 +8,28 @@ const app = express();
 // const path = require('path');
 
 // Middlewars imports
-const cors = require("cors");
-const bodyParser = require("body-parser");
+// const cors = require("cors");
+// const bodyParser = require("body-parser");
 
 // DB Import
 const mongoose = require("mongoose");
 
 // Middlewars
-app.use(cors());
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use(express.urlencoded({ extended: true }));
 
 //Enviroments variabels
-require('dotenv').config()
-const user = process.env.USER
-const pass = process.env.PASS
-const cluster = process.env.CLUSTER
+// require('dotenv').config()
+// const user = process.env.USER
+// const pass = process.env.PASS
+// const cluster = process.env.CLUSTER
 
-//DB connection
-const connection = mongoose.connect(`mongodb+srv://${user}:${pass}@${cluster}.gvpzqhx.mongodb.net/apas`)
-if(connection){
-    console.log('Conectado to MongoDB');
-}
+// //DB connection
+// const connection = mongoose.connect(`mongodb+srv://${user}:${pass}@${cluster}.gvpzqhx.mongodb.net/apas`)
+// if(connection){
+//     console.log('Conectado to MongoDB');
+// }
 
 // Rotas
 app.get('/', (req, res)=>{
